d07eec13f5ae29580d2b0ed33ffd5fa0fcacf18b
tlejoux <thibault.lejoux@epfedu.fr>
2022-09-14 10:59:43 +0200

    test

---
+++ Match_recuperation/Data_Parse.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+import imageio
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from awpy.parser import DemoParser
+from sklearn.datasets import make_blobs
+import math
+import seaborn as sns
+from faceit_api.faceit_data import FaceitData
+import requests
+import re
+import ast
+import zipfile
+import patoolib
+import os
+import time
+time.sleep(2)
+
+
+def match_recuperation_dict_txt(api_key="38b28095-4ca6-48b6-aec5-748f507d5fcf",
+                                player_id="57c4c556-3b8e-4695-bf55-122dde5040db", starting_item_position_call=0,
+                                return_items_call=2):
+    faceit_data = FaceitData(api_key)
+    all_match_player = faceit_data.player_matches(player_id=player_id, game="csgo",
+                                                  starting_item_position=starting_item_position_call,
+                                                  return_items=return_items_call)
+    list_of_match = []
+    succeed = 0
+    for i in range(len(all_match_player["items"])):
+       try:
+            verif = 0
+            print("_______________START DOWNLOADING MATCH NUMBER :", i, "___SUCCEED BEFORE : ", succeed)
+            faceit_data = FaceitData("38b28095-4ca6-48b6-aec5-748f507d5fcf")
+            print(all_match_player["items"][i]["match_id"])
+            match_details = faceit_data.match_details(all_match_player["items"][i]["match_id"])
+            match_name = all_match_player["items"][i]["match_id"]
+            for root, dirs, files in os.walk("C:/demo_csgo/DataBase/" + match_details["voting"]["map"]["pick"][0]):
+                for filename in files:
+                    # print(filename," compare to : ",match_details["voting"]["map"]["pick"][0]+"_"+str(match_details["configured_at"])+"_"+match_name+'.txt')
+                    if filename == match_details["voting"]["map"]["pick"][0] + "_" + str(
+                            match_details["configured_at"]) + "_" + match_name + '.json':
+                        verif = 1
+                        break
+            if verif == 1:
+                print("demo déjà présente, on passe à la suite")
+                continue
+            url = match_details["demo_url"][0]
+            headers = {
+                'accept': 'application/json',
+                'Authorization': 'Bearer {}'.format(api_key)
+            }
+            print(url)
+            r = requests.get(url)
+            #  print(r.status_code)
+            with open('C:/demo_csgo/DataBase/' + match_details["voting"]["map"]["pick"][0] + "_" + str(
+                    match_details["configured_at"]) + '.dem.7z', 'wb') as f:
+                f.write(r.content)
+            # print('C:/demo_csgo/DataBase/'+match_details["voting"]["map"]["pick"][0]+"_"+str(match_details["configured_at"])+'.dem.7z')
+            print('C:/demo_csgo/DataBase/' + match_details["voting"]["map"]["pick"][0] + "_" + str(
+                match_details["configured_at"]) + '.dem.7z')
+            patoolib.extract_archive('C:/demo_csgo/DataBase/' + match_details["voting"]["map"]["pick"][0] + "_" + str(
+                match_details["configured_at"]) + '.dem.7z', outdir="C:/demo_csgo/DataBase")
+            s = match_details["demo_url"][0]
+            pattern = "csgo/(.*?).dem"
+            match_name = re.search(pattern, s).group(1)
+
+            print("debut du parse")
+            demo_parser = DemoParser(demofile='C:/demo_csgo/DataBase/' + match_name + '.dem',
+                                     demo_id=str(match_details["configured_at"]), parse_rate=128)
+            data = demo_parser.parse()
+            print("parse success")
+
+            os.remove('C:/demo_csgo/DataBase/' + match_name + '.dem')
+            os.remove('C:/demo_csgo/DataBase/' + match_details["voting"]["map"]["pick"][0] + "_" + str(
+                match_details["configured_at"]) + '.dem.7z')
+            os.remove(str(match_details["configured_at"]) + ".json")
+            match_name = all_match_player["items"][i]["match_id"]
+
+            with open('C:/demo_csgo/DataBase/' + match_details["voting"]["map"]["pick"][0] + '/' +
+                      match_details["voting"]["map"]["pick"][0] + "_" + str(
+                    match_details["configured_at"]) + "_" + match_name + '.json', 'w') as json_file:
+                json.dump(data, json_file)
+            print(match_details["voting"]["map"]["pick"][0]+"_"+str(match_details["configured_at"])+"_"+match_name)
+            succeed += 1
+        except:
+            print("error, try next : ",r.status_code)
+            os.remove('C:/demo_csgo/DataBase/'+match_details["voting"]["map"]["pick"][0]+"_"+str(match_details["configured_at"])+'.dem.7z')
+            os.system('clear')
