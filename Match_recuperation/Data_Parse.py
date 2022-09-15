c9db3630216c9381beb0c8ffbc4b70c8d5f39621
tlejoux <thibault.lejoux@epfedu.fr>
2022-09-15 20:57:37 +0200

    - Analyse uniquement les parties avec les mêmes premades (nombre pris en compte est modifiable)

--- Match_recuperation/Data_Parse.py
+++ Match_recuperation/Data_Parse.py
@@ -25,7 +25,7 @@ def get_player_id(api_key="38b28095-4ca6-48b6-aec5-748f507d5fcf",nickname = "mem
 
 def match_recuperation_dict_txt(api_key="38b28095-4ca6-48b6-aec5-748f507d5fcf",
                                 player_id="57c4c556-3b8e-4695-bf55-122dde5040db", starting_item_position_call=0,
-                                return_items_call=2,nickname = "memetiti", map_select = None):
+                                return_items_call=2,nickname = "memetiti", map_select = None, premade=[]):
     if nickname != "memetiti":
         player_id = get_player_id(nickname = nickname)['player_id']
         print(nickname," : ",player_id)
@@ -39,11 +39,25 @@ def match_recuperation_dict_txt(api_key="38b28095-4ca6-48b6-aec5-748f507d5fcf",
     for i in range(len(all_match_player["items"])):
        cpt+=1
        try:
-            carte =faceit_data.match_details(match_id=all_match_player["items"][i]['match_id'])['voting']['map']['pick'][0]
+            match_detail = faceit_data.match_details(match_id=all_match_player["items"][i]['match_id'])
+            carte = match_detail['voting']['map']['pick'][0]
             print("_______________START DOWNLOADING MATCH NUMBER :",cpt, 'on map :', carte, "___SUCCEED BEFORE : ", succeed)
             if (map_select != None) & (carte != map_select) :
                 print("wrong map: ",carte,"vs", map_select)
                 continue
+
+            cpt = 0
+            nb_premade = len(premade)
+            print("Check premade : ")
+            for faction in ['faction1','faction2']:
+                for roster in match_detail['teams'][faction]['roster']:
+                    if (roster['nickname'] in premade):
+                        print(roster['nickname']," founded")
+                        cpt += 1
+            if cpt != nb_premade:
+                print("###il manque :", nb_premade - cpt, " premade###")
+                continue
+
             verif = 0
             faceit_data = FaceitData("38b28095-4ca6-48b6-aec5-748f507d5fcf")
             print(all_match_player["items"][i]["match_id"])
